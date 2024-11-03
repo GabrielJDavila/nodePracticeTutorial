0f149f04493fb6c7a991ee5b848695a4bb25c096
Gabriel Davila <gdavila0819@gmail.com>
2024-11-03 17:26:53 -0600

    more node.js practice. Working/learning about streams

--- app.mjs
+++ app.mjs
@@ -1,13 +1,29 @@
 import { createReadStream }  from 'node:fs'
+import http from 'node:http'
 
-const stream = createReadStream('./testfolder/big.txt', {highWaterMark: 90000, encoding: 'utf8'})
+http
+    .createServer((req, res) => {
+        const fileStream = createReadStream('./testfolder/big.txt', 'utf8')
+        fileStream.on('open', () => {
+            fileStream.pipe(res)
+        })
+        fileStream.on('error', (err) => {
+            res.end(err)
+            console.log('error reading stream: ', err)
+        })
+    })
+    .listen(5000, () => {
+        console.log('listening on http://localhost:5000/')
+    })
 
-stream.on('data', (result) => {
-    console.log(result)
-})
-stream.on('error', (err) => {
-    console.log('error reading stream: ', err)
-})
+// const stream = createReadStream('./testfolder/big.txt', {highWaterMark: 90000, encoding: 'utf8'})
+
+// stream.on('data', (result) => {
+//     console.log(result)
+// })
+// stream.on('error', (err) => {
+//     console.log('error reading stream: ', err)
+// })
 // import http from 'node:http'
 
 // const server = http.createServer()
