64b79ffcb850842166765e384adec6d45f486d21
Gabriel Davila <gdavila0819@gmail.com>
2024-11-02 20:52:10 -0500

    more node.js practice. Practicing async/sync code, the event loop, promises, and async/await

--- app.mjs
+++ app.mjs
@@ -1,8 +1,64 @@
-import lo from 'lodash'
+import { readFile, writeFile } from 'node:fs'
+import util from 'node:util'
 
-const items = [1, [2, [3, [4]]]]
+const readFilePromise = util.promisify(readFile)
+const writeFilePromise = util.promisify(writeFile)
 
-const newItems = lo.flattenDeep(items)
+const start = async() => {
+    try {
+        const first = await readFilePromise('./testfolder/first.txt', 'utf8')
+        const second = await readFilePromise('./testfolder/second.txt', 'utf8')
+        await writeFilePromise('./testfolder/write-new.txt', `${first} ${second}`, {flag: 'a'})
+        console.log(first, second)
+    } catch(err) {
+        console.error("error fetching: ", err)
+    }
+}
 
-console.log(newItems)
-console.log("hello brosss")
+start()
+
+// const getText = (path) => {
+//     return new Promise( (resolve, reject) => {
+//         readFile(path, 'utf8', (err, data) => {
+//             if(err) {
+//                 reject(err)
+//             } else {
+//                 resolve(data)
+//             }
+//         })
+//     })
+// }
+
+// const writeText = (path, first, second) => {
+//     return new Promise((resolve, reject) => {
+//         writeFile(path, `Here is the result ${first + second}`, (err, data) => {
+//             if(err) {
+//                 reject(err)
+//             } else {
+//                 resolve(data)
+//             }
+//         })
+//     })
+// }
+
+// getText('./testfolder/first.txt')
+//     .then(result => console.log(result))
+//     .catch(err => console.error('error getting text: ', err))
+
+// const server = http.createServer((req, res) => {
+//     if(req.url === "/") {
+//         res.end("Home Page")
+//     }
+//     if(req.url === "/about") {
+//         for(let i = 0; i < 1000; i++) {
+//             for(let j = 0; j < 1000; j++) {
+//                 console.log(`${i}, ${j}`)
+//             }
+//         }
+//         res.end("About Page")
+//     }
+// })
+
+// server.listen(5000, () => {
+//     console.log("Server running at http://localhost:5000/")
+// })
--- http.mjs
+++ http.mjs
@@ -1,22 +1,23 @@
-import http from 'node:http'
+// import http from 'node:http'
 
-const server = http.createServer((request, response) => {
-    if(request.url === '/') {
-        response.end("Welcome to our home page!")
-        return
-    }
-    if(request.url === '/about') {
-        response.end('here is the about page')
-        return
-    }
-    response.end(`
-        <h1>Oops!</h1>
-        <p>Page does not exist.</p>
-        <a href="/">Back to home</a>
-    `)
-    return
-})
+// const server = http.createServer((request, response) => {
+//     if(request.url === '/') {
+//         console.log("request event")
+//         response.end("Welcome to our home page!")
+//         return
+//     }
+//     if(request.url === '/about') {
+//         response.end('here is the about page')
+//         return
+//     }
+//     response.end(`
+//         <h1>Oops!</h1>
+//         <p>Page does not exist.</p>
+//         <a href="/">Back to home</a>
+//     `)
+//     return
+// })
 
-server.listen(3000, () => {
-    console.log('Server running at http://localhost:3000/')
-})
+// server.listen(3000, () => {
+//     console.log('Server running at http://localhost:3000/')
+// })
